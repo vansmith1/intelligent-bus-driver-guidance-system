1acf2457732f2cfd904fb4843a95b90f2aa4cc5b
Jaiden Heinzmann <s4094584@student.rmit.edu.au>
2026-06-01 17:46:35 +1000

    set up skeleton code for driverRepository + test implementation

---
+++ src/main/java/DriverRepository.java
@@ -0,0 +1,20 @@
+public class DriverRepository {
+    
+    private static final String DATABASE_PATH = "drivers.json";
+
+    public boolean add(Driver driver) {
+        return false;
+    }
+
+    public Driver retrieve(String driverID) {
+        return null;
+    }
+
+    public boolean update(Driver updatedDriver) {
+        return false;
+    }
+
+    public int count() {
+        return 0;
+    }
+}
---
+++ src/test/java/DriverRepositoryTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class DriverRepositoryTest {
+
+    @Test
+    void DriversAreStoredProperly() {}
+
+    @Test
+    void InvalidDriversAreRejected() {}
+
+    @Test
+    void UpdatesArePersistedCorrectly() {}
+
+    @Test
+    void RecordCountsAreUpdatedCorrectly() {}
+
+}
